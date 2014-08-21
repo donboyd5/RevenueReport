e544116889dcb093abe5fae726008bc705ac85f5
donboyd5 <donboyd5@gmail.com>
2014-08-21 17:06:29 -0400

    u

--- findneighbors.r
+++ findneighbors.r
@@ -2,7 +2,15 @@
 # Don Boyd
 # 8/21/2014
 
+# https://www.census.gov/geo/reference/county-adjacency.html
+
 ubase <- "https://www.census.gov/geo/reference/docs/"
 fn <- "county_adjacency.txt"
 
-download.file()
+download.file(paste0(ubase, fn), paste0(qtd, fn))
+
+
+# state centroids
+fn <- "CenPop2010_Mean_ST.txt"
+
+df <- read.csv(paste0(qtd, fn))
